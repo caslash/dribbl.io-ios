f44cd74e4adb50002b31f5c6ca8be9b3d11a2451
Cameron Slash <calexslash@aol.com>
2026-03-25 20:46:26 -0400

    Initial Commit

---
+++ dribblio-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dribblio-ios
+//
+//  Created by Cameron Slash on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dribblio-ios/dribblio_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dribblio_iosApp.swift
+//  dribblio-ios
+//
+//  Created by Cameron Slash on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct dribblio_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
